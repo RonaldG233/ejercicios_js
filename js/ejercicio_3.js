830e6147358b620164111ee740923617512acff9
ronaldG <ronaldalejandrogallocastellano@gmail.com>
2024-12-02 11:31:28 -0500

    trabajo cuarto ejercicio

--- js/ejercicio_3.js
+++ js/ejercicio_3.js
@@ -1,10 +1,15 @@
 let num1=prompt("ingresar primer numero: ");
 let num2=prompt("ingresar segundo numero: ");
 
-if (num1>num2 && num2>0) {
-    alert(num1/num2);
-}else if  (num2==0) {
-    alert("no se puede dividir por cero");
-}else{
-    alert("error, ingrese numeros validos para la division");
+try {
+    if (num1>num2 && num2>0) {
+        console.log(num1/num2);
+    }else if(num2==0){
+        throw("no se puede dividir por cero");
+    }else{
+        throw("error, ingrese numeros validos para la division");
+    }
+} catch (error) {
+    console.log(error);
+     
 }
---
+++ js/ejercicio_4.js
@@ -0,0 +1,9 @@
+let numero=prompt("Ingresar numero entero");
+if (numero % 2 === 0) {
+    alert("EL NUMERO ES PAR")
+    
+}else if (numero%1 !=0) {
+    alert("EL NUMERO ES DECIMAL")   
+}else{
+    alert("EL NUMERO ES IMPAR")
+}

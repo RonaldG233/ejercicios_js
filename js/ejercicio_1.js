ee7228d9e3cdcadda6da6849f4761ac4fbc0740d
ronaldG <ronaldalejandrogallocastellano@gmail.com>
2024-12-02 08:18:44 -0500

    desarrollo primer ejercicio

---
+++ js/ejercicio_1.js
@@ -0,0 +1,12 @@
+let edad=prompt("Ingrese su edad: ");
+console.log(edad);
+
+if (edad>0) {
+    if (edad>=18) {
+        console.log ("es mayor de edad");
+    }else{
+        console.log ("es menor de edad");
+    }
+}
+
+

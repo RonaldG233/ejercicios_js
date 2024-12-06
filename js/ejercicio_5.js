c11e6d83bba96db216286103f8d581fbb8875373
ronaldG <ronaldalejandrogallocastellano@gmail.com>
2024-12-06 07:19:19 -0500

    ejercicio 5 y 6

---
+++ js/ejercicio_5.js
@@ -0,0 +1,7 @@
+let edad=prompt("Ingrese su edad: ");
+let ingresos=prompt("Poner el ingreso que obtiene mensualmente: ");
+if (edad>16 && ingresos>=1000) {
+    alert("EL USUARIO DEBE TRIBUTAR POR SU MAYORIA DE EDAD, Y POR SUS INGRESOS MAYOR O IGUAL A 1000")
+}else{
+    alert("EL USUARIO NO DEBERA TRIBUTAR")
+}
---
+++ js/ejercicio_6.js
@@ -0,0 +1,13 @@
+let nombre=prompt("ingrese su nombre: ");
+let genero=prompt("ingrese su genero: ");
+
+let cursos=function (nombre, genero) {
+    if (genero==="f"  ) {
+        if (nombre=="m..") {
+            /[a-mA-M]/
+        }
+    }
+}
+
+console.log(cursos);
+

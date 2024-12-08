4f3aa0577453cfb74d83a12daa860350780d0329
ronaldgallo <ronaldalejandrogallocastellano@gmail.com>
2024-12-08 17:55:00 -0500

    ejercicio 18

---
+++ js/ejercicio_18.js
@@ -0,0 +1,4 @@
+let a=prompt("Ingresa numero que se va a considerar como A");
+let b=prompt("Ingresa numero que se va a considerar como B");
+let c=prompt("Ingresa numero que se va a considerar como C");
+alert("EL RESULTADO DE LA EXPRESION ES: " + ((a+7*c) / (b+2-a) + 2*b))

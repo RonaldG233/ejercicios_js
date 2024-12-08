b7407f4ff775a58efc96fcb2543e130f3929360c
ronaldgallo <ronaldalejandrogallocastellano@gmail.com>
2024-12-08 16:18:50 -0500

    ejercicio 13

---
+++ js/ejercicio_13.js
@@ -0,0 +1,8 @@
+let prim_num=prompt("Ingresar primer numero");
+let segund_num=prompt("Ingresar segundo numero");
+let terc_num=prompt("Ingresar tercero numero");
+let cuert_num=prompt("Ingresar cuarto numero");
+
+
+
+alert((prim_num+segund_num+terc_num+cuert_num)/4);

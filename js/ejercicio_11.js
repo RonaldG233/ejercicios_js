a78bfd9cf4e5f8e47754dc878972d6225803baa6
ronaldgallo <ronaldalejandrogallocastellano@gmail.com>
2024-12-08 15:13:00 -0500

    ejercicio 12

--- js/ejercicio_11.js
+++ js/ejercicio_11.js
@@ -1,13 +1,19 @@
 let num1=prompt("Ingresar primer numero");
 let num2=prompt("Ingresar segundo numero");
 let operaciones=function (num1, num2) {
-    alert("LA SUMA DE LA OPERACION ES: "+ num1+num2)
-    alert("LA RESTA DE LA OPERACION ES: "+num1-num2)
-    alert("LA MULTIPLICACION DE LA OPERACION ES: "+num1*num2)
+    let sum=num1+num2;
+    let resta=num1-num2;
+    let multiplicacion=num1*num2;
+    
+    alert(sum  )
+    alert("LA RESTA DE LA OPERACION ES: "+resta )
+    alert("LA MULTIPLICACION DE LA OPERACION ES: "+multiplicacion )
     if (num2==0) {
+        
         alert("NO SE PUEDE DIVIDIR POR CERO")
     }else{
-        alert("LA DIVISION DE LA OPERACION ES: "+num1/num2)
+        let division=num1/num2;
+        alert("LA DIVISION DE LA OPERACION ES: "+division )
     }
 }
 console.log(operaciones(num1, num2));
---
+++ js/ejercicio_12.js
@@ -0,0 +1,4 @@
+let largo=prompt("Ingrese el largo en centimetros del rectangulo: ");
+let ancho=prompt("Ingrese el ancho en centimetros del rectangulo: ");
+ alert("el area del rectangulo es: "+ancho*largo)
+

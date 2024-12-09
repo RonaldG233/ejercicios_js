84b0d1e16b2e4dd4dbd0da136e66347685cd7f45
ronaldG <ronaldalejandrogallocastellano@gmail.com>
2024-12-09 08:16:42 -0500

    ejercicio_26

---
+++ js/ejercicio_26.js
@@ -0,0 +1,4 @@
+//farenheit= celsius * (9/5) + 32
+let centigrados=parseInt(prompt("Ingresar un numero, que sea asignado a grados centigrados:  "));
+let farenheit=centigrados * (9/5) + 32;
+alert("EL RESULTADO EN GRADOS FARENHEIT ES: "+ (farenheit)+ " F")

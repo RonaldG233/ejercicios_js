c3b0ab50867788a4dcf2b47a8dac11f8e2ad825e
ronaldG <ronaldalejandrogallocastellano@gmail.com>
2024-12-09 07:09:52 -0500

    ejercicio_22

---
+++ js/ejercicio_22.js
@@ -0,0 +1,3 @@
+let minutos=parseInt(prompt("Ingresar los minutos que se ha demorado en la llamada: "));
+let costo_llamada=(355*minutos);
+alert("EL COSTO DE LA LLAMADA TELEFONICA INCLUYENDO EL IVA ES DE: "+ (costo_llamada+(costo_llamada*0.20)))

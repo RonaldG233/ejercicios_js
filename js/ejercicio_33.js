35a542d2fddf93b7f9ac961862f74cf5eccf2053
ronaldG <ronaldalejandrogallocastellano@gmail.com>
2024-12-09 10:36:01 -0500

    ejercicio_33

---
+++ js/ejercicio_33.js
@@ -0,0 +1,4 @@
+let edad_mujer=parseInt(prompt("Ingrese la edad actual de la mujer: "));
+let edad_hijo=parseInt(prompt("Ingese la edad del hijo de la mujer: "));
+let tiempo_pasado=(edad_mujer-edad_hijo);
+alert("LA EDAD DE LA MUJER A LA HORA DE TENER A SU HIJO FUE DE: "+tiempo_pasado)

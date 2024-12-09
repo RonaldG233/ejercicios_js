47fb926fbe22c2028ad9a35242ad85c7ca3377ff
ronaldG <ronaldalejandrogallocastellano@gmail.com>
2024-12-09 11:07:06 -0500

    ejercicio_35

---
+++ js/ejercicio_35.js
@@ -0,0 +1,6 @@
+let llaves_exagonales=11500*5;
+let bomba=1168000;
+let cajas_pernos=87000*3;
+let sobra=91000;
+let suma=(llaves_exagonales+bomba+cajas_pernos+sobra);
+alert("EL TOTAL DEL DINERO QUE TENIA LA PERSONA ERA DE: "+(suma))

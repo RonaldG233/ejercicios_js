64745a3b545dc9cd9c827c075152162baf8cebb0
ronaldG <ronaldalejandrogallocastellano@gmail.com>
2024-12-09 06:52:31 -0500

    ejercicio_21

---
+++ js/ejercicio_21.js
@@ -0,0 +1,5 @@
+let alto=parseInt(prompt("Ingresar el alto del elipsoide:"));
+let largo=parseInt(prompt("Ingresar el alto del elipsoide:"));
+let ancho=prompt("Ingresar el alto del elipsoide:");
+let volumen_elipsoide= ((3/4*Math.PI)*(alto*largo*ancho));
+alert("El volumen del elipoide es: "+  (volumen_elipsoide))

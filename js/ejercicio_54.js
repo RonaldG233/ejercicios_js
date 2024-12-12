5e1c598bd6b8eb5d08527f943a8ae1ad0bae4ab2
ronaldgallo <ronaldalejandrogallocastellano@gmail.com>
2024-12-12 15:55:45 -0500

    ejercicio 54

---
+++ js/ejercicio_54.js
@@ -0,0 +1,7 @@
+let primer_numero=parseInt(prompt("Ingrese el primer numero: "));
+let segundo_numero=parseInt(prompt("Ingrese el segundo numero: "));
+if (primer_numero>segundo_numero) {
+    alert("El resultado de la resta es de: "+(primer_numero-segundo_numero))
+}else{
+    alert("La operacion no es posible realizarla")
+}

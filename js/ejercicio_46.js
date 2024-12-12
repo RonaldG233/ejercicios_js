49c5bed245f43dea5fb942148311c281beb7f580
ronaldgallo <ronaldalejandrogallocastellano@gmail.com>
2024-12-12 14:33:31 -0500

    ejercicio 46

---
+++ js/ejercicio_46.js
@@ -0,0 +1,10 @@
+let a=parseInt(prompt("Ingrese numero A: "));
+let b=parseInt(prompt("Ingrese numero B: "));
+let c=parseInt(prompt("Ingrese numero C: "));
+if (a>b && a>c) {
+    alert("EL NUMERO MAYOR ES A")
+}else if (b>a && b>c) {
+    alert("EL NUMERO MAYOR ES B")
+}else if (c>a && c>b) {
+    alert("EL NUMERO MAYOR ES C")
+}

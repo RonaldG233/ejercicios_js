3ed283b192960eb746b444c515030fa25e24bc14
ronaldgallo <ronaldalejandrogallocastellano@gmail.com>
2024-12-12 14:25:35 -0500

    ejercicio 45

---
+++ js/ejercicio_45.js
@@ -0,0 +1,7 @@
+let numero=parseInt(prompt("Ingresar numero: "));
+let determina=function (numero) {
+    if (numero>0 && numero<100) {
+        alert("ES UN NUMERO POSITIVO Y ES UN NUMERO MENOR QUE 100")
+}
+}
+console.log(determina(numero));

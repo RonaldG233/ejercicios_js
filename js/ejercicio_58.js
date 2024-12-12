ec25e4002729cf3553cb9b3f945bb3f193141cca
ronaldgallo <ronaldalejandrogallocastellano@gmail.com>
2024-12-12 18:02:46 -0500

    ejercicio 58

---
+++ js/ejercicio_58.js
@@ -0,0 +1,26 @@
+let mes=prompt("Ingresar nombre del mes: ");
+if (mes==="enero") {
+    alert("SU SIGNO ZODIACAL ES CAPRICORNIO")
+}else if (mes==="febrero") {
+    alert("SU SIGNO ZODIACAL ES ACUARIO")
+}else if (mes==="marzo") {
+    alert("SU SIGNO ZODIACAL ES PISCIS")
+}else if (mes==="abril") {
+    alert("SU SIGNO ZODIACAL ES ARIES")
+}else if (mes==="mayo") {
+    alert("SU SIGNO ZODIACAL ES TAURO")
+}else if (mes==="junio") {
+    alert("SU SIGNO ZODIACAL ES GEMINIS")
+}else if (mes==="julio") {
+    alert("SU SIGNO ZODIACAL ES CANCER")
+}else if (mes==="agosto") {
+    alert("SU SIGNO ZODIACAL ES LEO")
+}else if (mes==="septiembre") {
+    alert("SU SIGNO ZODIACAL ES VIRGO")
+}else if (mes==="octubre") {
+    alert("SU SIGNO ZODIACAL ES LIBRA")
+}else if (mes==="noviembre") {
+    alert("SU SIGNO ZODIACAL ES ESCORPIO")
+}else if (mes==="diciembre") {
+    alert("SU SIGNO ZODIACAL ES SAGITARIO")
+}

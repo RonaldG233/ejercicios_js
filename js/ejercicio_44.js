48341ed8b7f840ca5b0de3f24aca3311bcd7971f
ronaldgallo <ronaldalejandrogallocastellano@gmail.com>
2024-12-12 14:14:59 -0500

    ejercicio 44

---
+++ js/ejercicio_44.js
@@ -0,0 +1,5 @@
+let pesos=parseFloat(prompt("Ingrese el valor en pesos: "));
+let dolares=pesos/4500;
+let euros=pesos/5000;
+alert("EL VALOR EN DOLARES SERIA: " +(dolares) + " USD")
+alert("EL VALOR EN EUROS SERIA: " +(euros)+ " EUR")

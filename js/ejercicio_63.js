a1405795093914625eae8648ab7739e317b3867e
ronaldgallo <ronaldalejandrogallocastellano@gmail.com>
2024-12-12 21:43:39 -0500

    ejercicio 63

---
+++ js/ejercicio_63.js
@@ -0,0 +1,8 @@
+let edad=parseInt(prompt("Ingrese la edad del usuario: "));
+for (let i = 1; i <=edad; i++) {
+    if (i==1)
+    alert(`HA CUMPLIDO ${i} año` )
+    if (i>=2) {
+        alert(`HA CUMPLIDO ${i} años` )
+    }
+}

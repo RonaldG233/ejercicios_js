22c15d74fb93015976d8a3a9281bf4c57de8f3c0
ronaldgallo <ronaldalejandrogallocastellano@gmail.com>
2024-12-13 17:06:16 -0500

    ejercicio 89

---
+++ js/ejercicio_89.js
@@ -0,0 +1,14 @@
+function verificarParImpar() {
+   
+    let numero = Math.floor(Math.random() * 10) + 1;
+
+   
+    if (numero % 2 === 0) {
+        alert("El número " + numero + " es par.");
+    } else {
+        alert("El número " + numero + " es impar.");
+    }
+}
+
+
+verificarParImpar();

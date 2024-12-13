080d7cc37d38c97419697396063494644e1bd7cd
ronaldgallo <ronaldalejandrogallocastellano@gmail.com>
2024-12-12 22:37:50 -0500

    ejercicio 68

---
+++ js/ejercicio_68.js
@@ -0,0 +1,5 @@
+let tabla=parseInt(prompt("Ingrese un numero para mostrar la tabla multiplicar de ese numero:  "));
+for (let i = 1; i <=10; i++) {
+    alert(tabla +"*" + i + " = " + tabla*i)
+    
+}
